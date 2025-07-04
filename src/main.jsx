7f5f42f32272aaa6c9b17db7ea940edcdab1c330
ojasbhosale <ojasbhosale07@gmail.com>
2025-07-04 23:40:48 +0530

    remove unused performance measurement comments from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -18,5 +18,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
